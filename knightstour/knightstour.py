6e165fd97d25fb1835b3798f4660688854721b96
Raycho Mukelov <raicho.mukelov@gmail.com>
2021-07-24 00:53:00 +0300

    Comment

--- knightstour/knightstour.py
+++ knightstour/knightstour.py
@@ -118,7 +118,8 @@ class KnightsTourAlgo:
             Compute path's "matrix context pattern" - path nodes are encoded as single bits in a integer.
             The position of the bits set to "1" is relative to the path nodes coordinates.
             This represents the pattern of the given path ignoring the order of the nodes in it meaning that the reversed path will have the same
-            matrix pattern and so on. This enables very fast searches of the paths already known to be with a dead end.
+            matrix pattern and so on. This enables fast searches of the paths already known to be with a dead end with minimum required space.
+            Keep in mind we just store some integers in a set().
         """
         mtx_ctx = 0
         b = [(path_node[1] * self.board_size + path_node[0]) for path_node in path]
